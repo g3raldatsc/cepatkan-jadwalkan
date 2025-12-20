a0553154f31c2c469f6c834b746366b8cc24ac12
Excelyno Magenta <excellentqweee@gmail.com>
2025-12-20 23:22:13 +0700

    Remove sensitive firebase configuration
    Removed firebase configuration details for security.

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,5 @@
 // Konfigurasinya
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBocDPFYJhdGkCgPW7oDECyqEiFs3GOF1U",
-  authDomain: "cepatkan-jadwalkan.firebaseapp.com",
-  projectId: "cepatkan-jadwalkan",
-  storageBucket: "cepatkan-jadwalkan.firebasestorage.app",
-  messagingSenderId: "138163356272",
-  appId: "1:138163356272:web:38ccec0ff404588dc72ab5"
-};
 
 // Inisialisasinya
 
