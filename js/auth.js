aa3ba36e3ae67f84ee7dcab5e24ff67d39be7e57
Gerald Jepedro Sitorus <pedrositorur@gmail.com>
2025-12-23 18:30:56 +0700

    auth change

--- js/auth.js
+++ js/auth.js
@@ -4,7 +4,16 @@ function masukGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider)
         .then((result) => {
-            console.log("Login sukses:", result.user.displayName);
+            const user = result.user;
+            console.log("Login sukses:", user.displayName);
+
+            db.collection("users").doc(user.uid).set({
+                nama: user.displayName,
+                email: user.email,
+                foto: user.photoURL,
+                terakhirLogin: firebase.firestore.FieldValue.serverTimestamp()
+            }, { merge: true }); 
+
         })
         .catch((error) => {
             alert("Gagal masuk: " + error.message);
