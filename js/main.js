e6ba6db8e8fba727729c2407eea59d75682bd410
Gerald Jepedro Sitorus <pedrositorur@gmail.com>
2025-12-23 23:42:22 +0700

    Sedikit perubahan

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // js/main.js
 
 // Ambil elemen dom
+
 const tombolMasuk = document.getElementById("tombol-masuk-google");
 const tombolKeluar = document.getElementById("tombol-keluar");
 const halamanMasuk = document.getElementById("halaman-masuk");
@@ -18,7 +19,6 @@ if(tombolKeluar) tombolKeluar.addEventListener("click", keluarSistem);
 auth.onAuthStateChanged((user) => {
     
     if (user) {
-        // === USER SUDAH LOGIN ===
         // Set data profil dulu di background
         const namaDepan = user.displayName ? user.displayName.split(" ")[0] : "User";
         teksSapaan.innerText = namaDepan;
@@ -30,24 +30,19 @@ auth.onAuthStateChanged((user) => {
         muatJadwalBeranda();
         gantiMenu('beranda');
 
-        // Tampilkan Dashboard, Sembunyikan Login
         halamanMasuk.style.display = "none";
         halamanDashboard.style.display = "block";
 
     } else {
-        // === USER BELUM LOGIN ===
-        // Tampilkan Login, Sembunyikan Dashboard
         halamanDashboard.style.display = "none";
         halamanMasuk.style.display = "flex";
     }
 
-    // === HILANGKAN LOADING SCREEN ===
-    // Beri sedikit jeda biar transisi mulus
     if(loadingScreen) {
         setTimeout(() => {
-            loadingScreen.style.opacity = '0'; // Fade out
+            loadingScreen.style.opacity = '0'; 
             setTimeout(() => {
-                loadingScreen.style.display = 'none'; // Hilang total
+                loadingScreen.style.display = 'none'; 
             }, 500);
         }, 500);
     }
