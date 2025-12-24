cf789daaa5ce9b3b6f452e72f2abe00ed7eeff2b
excelyno <excellentqweee@gmail.com>
2025-12-24 23:17:51 +0700

    menghapus fungsi yang ada didalam ui

--- js/ui.js
+++ js/ui.js
@@ -1,24 +1,5 @@
 // js/ui.js
 
-function gantiMenu(menu) {
-    document.getElementById('fitur-beranda').style.display = 'none';
-    document.getElementById('fitur-atur').style.display = 'none';
-    document.getElementById('fitur-profil').style.display = 'none';
-    
-    document.getElementById('fitur-' + menu).style.display = 'block';
-
-    const navItems = document.querySelectorAll('.nav-item');
-    navItems.forEach(item => item.classList.remove('active'));
-
-    if(menu === 'beranda') navItems[0].classList.add('active');
-    if(menu === 'profil') navItems[1].classList.add('active');
-
-    if (menu === 'atur') {
-        muatJadwalEdit(); 
-    } else if (menu === 'beranda') {
-        muatJadwalBeranda(); 
-    }
-}
 
 function updateTanggal() {
     const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
